0feda3bfd7f89f8b050972f1e25eaf5ead89a303
Wilson Mália <wabmalia@gmail.com>
2019-12-03 07:56:41 +0000

    Improve and clean Day 2 part one solution

--- day_2/lib/program_alarm.ex
+++ day_2/lib/program_alarm.ex
@@ -5,58 +5,27 @@ defmodule Day2 do
     |> Enum.map(&String.to_integer(&1))
   end
 
-  def op_code(program, operation) do
-    case Enum.at(operation, 0) do
-      1 ->
-        IO.puts("Add")
-        index = Enum.at(operation, 3)
-        value = Enum.at(program, Enum.at(operation, 1)) + Enum.at(program, Enum.at(operation, 2))
-        program = List.insert_at(program, index, value)
-        Enum.each(program, &IO.puts(&1))
-
-      # IO.puts(value)
-      2 ->
-        # program[operation[3]] = program[operation[1]] * program[operation[2]]
-        IO.puts("Multiply")
-
-      99 ->
-        IO.puts("Finish")
-    end
-  end
-
-
-  def compute(program, index) do
-    IO.puts("Index: #{index}")
+  def execute(index, program) do
+    # IO.puts("Index: #{index}")
     operation = Enum.at(program, index)
-    IO.puts("Operation: #{operation}")
+    # IO.puts("Operation: #{operation}")
     cond do
-      operation == 99 -> hd program
-        # IO.puts("Value at head:#{Enum.at(program, 0)}")
+      operation == 99 -> {:halt, hd program}
       operation ->
         x = Enum.at(program, index + 1) |> (&(Enum.at(program, &1))).()
         y = Enum.at(program, index + 2) |> (&(Enum.at(program, &1))).()
-        position = Enum.at(program, index + 3) #|> (&(Enum.at(program, &1))).()
-        IO.puts("#{x}:#{y} #{position}")
+        position = Enum.at(program, index + 3)
+        # IO.puts("#{x}_#{y}_#{position}")
         case operation do
-          1 -> List.replace_at(program, position, x + y)
-          2 -> List.replace_at(program, position, x * y)
+          1 -> {:cont, List.replace_at(program, position, x + y)}
+          2 -> {:cont, List.replace_at(program, position, x * y)}
         end
     end
   end
 
   def computer(program) do
-    Stream.unfold(0, fn
-      x when x >= length(program) -> nil
-      x -> {x, x + 4}
-    end)
-    |> Enum.reduce_while(program,
-          fn x, acc ->
-            if is_list(acc) do
-              {:cont, compute(acc, x)}
-            else
-              {:halt, acc}
-            end
-          end)
+    Stream.unfold(0, fn x -> {x, x + 4} end)
+    |> Enum.reduce_while(program, fn x, acc -> execute(x, acc) end)
   end
 
   def part_one do
