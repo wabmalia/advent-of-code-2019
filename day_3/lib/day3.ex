a3c7e7cb76d3c9899012727c77cefd131a9d9497
Team Raven <team-raven@vw-dilab.com>
2019-12-06 14:26:32 +0000

    Add solution to day 3 part two puzzle

--- day_3/lib/day3.ex
+++ day_3/lib/day3.ex
@@ -7,21 +7,14 @@ defmodule Day3 do
 
   def to_coordinates(wire) do
     Enum.reduce(wire, [{0, 0}], fn action, acc ->
-      acc ++ [to_coordinate(List.last(acc), action)]
+      acc ++ [to_coordinate(action, List.last(acc))]
     end)
   end
 
-  def to_coordinate({x, y}, action) do
-    direction = action |> String.first()
-    movement = action |> String.slice(1..-1) |> String.to_integer()
-
-    case direction do
-      "U" -> {x, y + movement}
-      "D" -> {x, y - movement}
-      "R" -> {x + movement, y}
-      "L" -> {x - movement, y}
-    end
-  end
+  defp to_coordinate("U" <> number, {x, y}), do: {x, y + String.to_integer(number)}
+  defp to_coordinate("D" <> number, {x, y}), do: {x, y - String.to_integer(number)}
+  defp to_coordinate("R" <> number, {x, y}), do: {x + String.to_integer(number), y}
+  defp to_coordinate("L" <> number, {x, y}), do: {x - String.to_integer(number), y}
 
   def is_between(value, range_a, range_b) do
     value <= max(range_a, range_b) && value >= min(range_a, range_b)
@@ -52,16 +45,10 @@ defmodule Day3 do
     end
   end
 
-  def is_wires_crossing([wire_a, wire_b]) do
-    IO.inspect(wire_a)
-    IO.inspect(wire_b)
-    IO.inspect("-----")
-
-    extract_segments = fn segment -> [segment] end
-
-    manhattan_distance = fn {crossed, {x, y}} ->
+  def find_intersection([wire_a, wire_b]) do
+    acc_intersection = fn {crossed, {x, y}} ->
       if crossed && x + y > 0 do
-        [x + y]
+        [{x, y}]
       else
         []
       end
@@ -70,17 +57,44 @@ defmodule Day3 do
     compare_segment = fn segment_a ->
       fn segment_b ->
         crossing_point(segment_a, segment_b)
-        |> manhattan_distance.()
+        |> acc_intersection.()
       end
     end
 
-    trace_wire(wire_a, extract_segments)
+    trace_wire(wire_a, fn segment -> [segment] end)
     |> Enum.flat_map(fn segment -> trace_wire(wire_b, compare_segment.(segment)) end)
-    |> Enum.min()
+  end
+
+  def vector_distance({x_a, y_a}, {x_b, y_b}) do
+    floor(:math.sqrt(:math.pow(x_b - x_a, 2) + :math.pow(y_b - y_a, 2)))
+  end
+
+  def calculate_distance([head | tail], point, acc \\ 0) do
+    if length(tail) == 0 do
+      acc
+    else
+      case crossing_point({head, hd(tail)}, {point, point}) do
+        {crossed, intersection} when crossed ->
+          acc + vector_distance(head, intersection)
+
+        {false, _} ->
+          calculate_distance(tail, point, acc + vector_distance(head, hd(tail)))
+      end
+    end
   end
 
   def part_one do
     get_input("input")
-    |> is_wires_crossing()
+    |> find_intersection()
+    |> Stream.map(fn {x, y} -> x + y end)
+    |> Enum.min()
+  end
+
+  def part_two do
+    [wire_a, wire_b] = get_input("input")
+
+    find_intersection([wire_a, wire_b])
+    |> Enum.map(&(calculate_distance(wire_a, &1) + calculate_distance(wire_b, &1)))
+    |> Enum.min()
   end
 end
