859b504a963a9dea600765e135a94c080b98a09c
Wilson Mália <wabmalia@gmail.com>
2019-12-08 14:30:33 +0000

    Add solution to day 4 part two

--- day_4/password.ex
+++ day_4/password.ex
@@ -1,20 +1,42 @@
 defmodule Password do
-  def condition(curr, {last, has_doubles, is_increasing}) do
-    cond do
-      last == curr -> {:cont, {curr, true, is_increasing}}
-      last < curr -> {:cont, {curr, has_doubles, true}}
-      true -> {:halt, {curr, false, false}}
-    end
-  end
-
-  def filter(password) do
+  def filter(password, condition, evaluation) do
     Integer.to_charlist(password)
-    |> Enum.reduce_while({0, false, false}, &condition(&1, &2))
-    |> (fn {_, has_doubles, is_increasing} -> is_increasing && has_doubles end).()
+    |> Enum.reduce_while({0, false, [0]}, &condition.(&1, &2))
+    |> evaluation.()
   end
 end
 
-Stream.filter(153517..630395, &Password.filter(&1))
+condition = fn (curr, {last, is_increasing, match_count}) ->
+  [head | tail] = match_count
+  cond do
+    last == curr -> {:cont, {curr, is_increasing, [(head + 1)] ++ tail}}
+    last < curr -> {:cont, {curr, true, [0] ++ match_count}}
+    true -> {:halt, {curr, false, match_count}}
+  end
+end
+
+evaluation_one = fn ({_, is_increasing, match_count}) ->
+  is_increasing && Enum.any?(match_count, &(&1 > 0))
+end
+
+IO.inspect("Day 4")
+IO.inspect("Part One:")
+
+Stream.filter(153517..630395,
+&Password.filter(&1, condition, evaluation_one))
 |> Enum.count()
 |> IO.inspect()
+
+evaluation_two = fn ({_, is_increasing, match_count}) ->
+  is_increasing && Enum.any?(match_count, fn x -> x == 1 end)
+end
+
+IO.inspect("Part Two:")
+
+Stream.filter(153517..630395,
+  &Password.filter(&1, condition, evaluation_two))
+|> Enum.count()
+|> IO.inspect()
+
+
 
